ceef8697e71cc6c0a066e9a60004fff6ed316685
Abdulsaboorhabibi <saboorhabibi10@gmail,com>
2024-03-13 12:17:33 +0430

    Python version Check file is added.

---
+++ Code/Check_version.py
@@ -0,0 +1,21 @@
+"""
+ONE:
+    How to check a python version
+
+"""
+
+import sys
+
+print(sys.version)
+
+"""
+
+TWO:
+    check python version in terminal 
+    
+    In Windows: 
+        python --version
+    In Mac: 
+        python3 --version
+
+"""
