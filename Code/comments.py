8ba44ca7ce4361af2ecb6150faaa103c19d26ad7
Abdulsaboorhabibi <saboorhabibi10@gmail,com>
2024-03-13 12:20:42 +0430

    Python version Comments file is added.

---
+++ Code/comments.py
@@ -0,0 +1,34 @@
+# in python programming language, we have three types of Comments
+# first tha we are writing which is start with '#' symbol
+
+# the second is the inline or end line comment when we write a statement in python than we create a comment like this
+print("Hello World!")  # this will print a hello world in console, So this is called inline or end line comment
+
+# the third type is multiline comment which comment's more than one lines
+
+# multiline
+# multiline
+# multiline     and more
+
+# but for multiline comment we have another easy-to-write method like this
+
+"""
+    with triple Double cots Or single cots we 
+    create multiline comment 
+    that is work perfectly for 
+    comment's that is more than one line for a developer
+"""
+
+# EXAMPLES
+
+# This line of code will print a hello world!
+print("Hello World!")
+
+
+print("Inline or end line comment")  # this will print end line comment
+
+'''
+    multiline comment will print that in multiline comment 
+'''
+
+print("The above comment is multiline comment.")
